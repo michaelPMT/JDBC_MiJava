5e4c97203a062ef4cf2e71f1f413e801eb8d3283
michaelPMT <michael_patrick_96@hotmail.cm>
2016-03-01 13:05:52 -0500

    ac

--- Clase01/AprendiendoJDBC/src/pe/egcc/prueba/prueba02.java
+++ Clase01/AprendiendoJDBC/src/pe/egcc/prueba/prueba02.java
@@ -2,21 +2,31 @@ package pe.egcc.prueba;
 import java.sql.Connection;
 import java.sql.Statement;
 public class prueba02 {
-    public static void main(String[] args) {
-        try {
-            Connection cn = AccesoDB.getConnection();
-            System.out.println("Conexion ok.");
-            
-            String sql = "Insert  into parametro(CHR_PARACODEIGO, "+ "VCH_PARA DESCRIPCION,VCH_PARAVALOR"
-                    + "VCH_PARAESTADO) VALUES('777','PROFESOR',"
-                    + "'gUSTAVO CORONEL','aCTIVO'";
-            Statement stm = cn.createStatement();
-            stm.executeUpdate("Parametro registrado");
-            
-            cn.close();
-            System.out.println("Conexion cerrada");
-        } catch (Exception e) {
-            System.out.println("Error: "+ e.getMessage());
-        }
+
+  public static void main(String[] args) {
+    Connection cn = null;
+    try {
+      cn = AccesoDB.getConnection();
+      System.out.println("Conxión ok.");
+
+      String sql = "insert into parametro(CHR_PARACODIGO,"
+              + "VCH_PARADESCRIPCION,VCH_PARAVALOR,"
+              + "VCH_PARAESTADO) values('776','PROFESOR',"
+              + "'GUSTAVO CORONEL','ACTIVO')";
+      Statement stm = cn.createStatement();
+      stm.executeUpdate(sql);
+      System.out.println("Parámetro registrado.");
+      
+      
+    } catch (Exception e) {
+      System.err.println("Error: " + e.getMessage());
+      
+    } finally{
+      try {
+        cn.close();
+        System.out.println("Conexión cerrada.");
+      } catch (Exception e) {
+      }
     }
+  }
 }
